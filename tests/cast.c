1b076d840d4b1e83443c617b2dd6e3c025d7e79b
Akash Kothari <jeff@vpnpool-130-126-152-18.near.illinois.edu>
2020-02-16 22:38:26 -0600

    Added tests

---
+++ tests/cast.c
@@ -0,0 +1,17 @@
+struct RT {
+    int *v;
+    int *u;
+};
+​
+struct ST {
+    float *p;
+    float *q;
+};
+​
+int main () {
+    struct ST st;
+    struct RT rt;
+    st.p = (float *)rt.u;
+​   rt.q = (float *)rt.v;
+    return 0;
+}
---
+++ tests/cast_struct.c
@@ -0,0 +1,17 @@
+struct RT {
+    int v;
+    int u;
+};
+​
+struct ST {
+    float *p;
+    float *q;
+};
+​
+int main () {
+    struct ST *st;
+    struct RT *rt;
+    st.p = (float *)v;
+    st.q = (float *)u;
+    return 0;
+}
