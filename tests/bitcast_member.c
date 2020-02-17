9e5783371b74dbf1d7e20752eae261707f1c09b7
Akash Kothari <jeff@vpnpool-130-126-152-18.near.illinois.edu>
2020-02-16 23:21:23 -0600

    Added tests

---
+++ tests/bitcast_member.c
@@ -0,0 +1,15 @@
+struct RT {
+    int v;
+    int u;
+};
+struct ST {
+    int p;
+    int q;
+};
+int main () {
+    struct ST *st;
+    struct RT *rt;
+    st->p = ((struct ST*)rt->v);
+    
+    return 0;
+}
