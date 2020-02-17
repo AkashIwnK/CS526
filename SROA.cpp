f2e94755e7726874f7b91c0ffca33984740ff26a
Akash Kothari <jeff@vpnpool-130-126-152-18.near.illinois.edu>
2020-02-16 21:56:27 -0600

    Update

--- SROA.cpp
+++ SROA.cpp
@@ -318,7 +318,7 @@ static bool AnalyzeAlloca(AllocaInst *AI, SmallVector<AllocaInst *, 4> &Worklist
         NumReplaced++;
         
         // Replace the uses of this GEP with the new Alloca
-       std::vector<BitCast *> BitCastVect;
+       std::vector<BitCastInst *> BitCastVect;
         for(auto *GEP : GEPVect) {
             errs() << "CONSIDERED GEP: " << *GEP << "\n";
             auto *GEPTy = GEP->getPointerOperand()->getType();
